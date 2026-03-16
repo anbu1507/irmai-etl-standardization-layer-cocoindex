61222cbbc7f12225e3354f01c3a04b5ac23a3a3c
Anbu M <anbu@irmai.io>
2026-03-16 13:31:38 +0000

    Edit main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,61 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "36448a90-905c-4f48-b1b3-deb171f7c247"
+# --- DATA SOURCES ---
+# 1. Reference the existing Resource Group
+data "azurerm_resource_group" "existing" {
+  name = var.resource_group_name
 }
 
-data "azurerm_storage_account" "storage" {
-  name                = "irmaiuatstorage"
-  resource_group_name = "rg-irmai-uat-us-1"
+# 2. Reference the existing Storage Account
+data "azurerm_storage_account" "existing" {
+  name                = var.storage_account_name
+  resource_group_name = data.azurerm_resource_group.existing.name
 }
 
-data "azurerm_service_plan" "standardization_plan" {
-  name                = "standardization-service-plan"
-  resource_group_name = "rg-irmai-uat-us-1"
+# --- RESOURCES ---
+
+# 3. Create the Service Plan (Renamed to v2 to avoid 'Already Exists' error)
+resource "azurerm_service_plan" "plan" {
+  name                = "sp-irmai-standardization-v2" 
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  os_type             = "Linux"
+  sku_name            = "B1" # Basic SKU to bypass regional dynamic limits
 }
 
-resource "azurerm_linux_function_app" "standardization_func" {
-  # WE ARE KEEPING THE NAME VIGNESH APPROVED
-  name                = "standardization-engine-anbu-final" 
-  resource_group_name = "rg-irmai-uat-us-1"
-  location            = "centralus"
-  
-  storage_account_name       = data.azurerm_storage_account.storage.name
-  storage_account_access_key = data.azurerm_storage_account.storage.primary_access_key
-  service_plan_id            = data.azurerm_service_plan.standardization_plan.id
+# 4. Create the Function App (The Engine)
+resource "azurerm_linux_function_app" "standardization_engine" {
+  name                = var.function_app_name
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
 
-  site_config {
-    application_stack {
-      python_version = "3.11"
-    }
-  }
+  storage_account_name       = data.azurerm_storage_account.existing.name
+  storage_account_access_key = data.azurerm_storage_account.existing.primary_access_key
+  service_plan_id            = azurerm_service_plan.plan.id
 
+  # Enable Managed Identity for professional security
   identity {
     type = "SystemAssigned"
   }
 
-  app_settings = {
-    "ENABLE_ORYX_BUILD"              = "true"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
-    "AzureWebJobsStorage"            = data.azurerm_storage_account.storage.primary_connection_string
-    
-    # Matches the connection name in your Python code
-    "MyStorageConn"                  = data.azurerm_storage_account.storage.primary_connection_string
-    
-    # --- BYPASSES 403 FORBIDDEN ---
-    # This prevents Azure from trying to create a new File Share
-    "WEBSITE_RUN_FROM_PACKAGE"       = "1"
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
   }
+
+  app_settings = {
+    # --- 403 FORBIDDEN FIXES (Bypasses Azure File Share creation) ---
+    "WEBSITE_RUN_FROM_PACKAGE"                  = "1"
+    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = "" 
+    "WEBSITE_CONTENTSHARE"                      = ""
+    
+    # --- APP CONFIG ---
+    "FUNCTIONS_WORKER_RUNTIME" = "python"
+    "MyStorageConn"            = data.azurerm_storage_account.existing.primary_connection_string
+    "AzureWebJobsStorage"      = data.azurerm_storage_account.existing.primary_connection_string
+  }
+}
+
+# 5. Output the Managed Identity ID (Principal ID)
+output "function_app_identity_principal_id" {
+  value = azurerm_linux_function_app.standardization_engine.identity[0].principal_id
 }
