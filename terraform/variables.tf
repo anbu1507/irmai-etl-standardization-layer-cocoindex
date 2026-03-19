1045fc1cbce8545cf22b7b08c3415317506ab8a6
Anbu M <anbu@irmai.io>
2026-03-19 12:28:31 +0000

    Add new file

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "rg-irmai-uat-us-1"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "functions_resource_group_name" {
+  default = "rg-irmai-standardization-fn"
+}
+
+variable "storage_account_name" {
+  default = "irmaiuatstorage"
+}
+
+variable "function_app_name" {
+  default = "irmai-standardization-engine-v1"
+}
